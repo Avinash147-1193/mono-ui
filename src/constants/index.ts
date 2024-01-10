79588b205eff4a2c0f7c89fc3b11721e72177925
Avinash Dwivedi <avinash147.1192@gmail.com>
2024-01-10 19:56:52 +0530

    initial commit

---
+++ src/constants/index.ts
@@ -0,0 +1,51 @@
+// Font families used in the app
+export const Fonts = {
+  Regular: "YourRegularFont",
+  Bold: "YourBoldFont",
+};
+
+// Media types used in the app
+export const Media = {
+  Icons: {
+    HomeIcon: require("../assets/icons/home.png"),
+  },
+  Images: {
+    Logo: require("../assets/images/logo.png"),
+  },
+};
+
+export const Colors = {
+  Primary: "#0A84FF",
+  Secondary: "#FF6D00",
+  // Add more color values as needed
+};
+
+// Dimensions used in the app
+export const Dimensions = {
+  ScreenWidth: 360,
+  ScreenHeight: 640,
+  // Add more dimension values as needed
+};
+
+// API endpoints used in the app
+export const APIEndpoints = {
+  BaseURL: "https://api.example.com/",
+};
+
+export const AnimationDurations = {
+  Short: 300,
+  Medium: 500,
+  Long: 800,
+  // Add more animation durations as needed
+};
+
+// Export all the constants
+export default {
+  Fonts,
+  Media,
+  Colors,
+  Dimensions,
+  APIEndpoints,
+  AnimationDurations,
+  // OtherConstants,
+};
